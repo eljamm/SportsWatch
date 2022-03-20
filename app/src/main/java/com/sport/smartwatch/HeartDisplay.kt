f8c266cf7eb179f84bb816447c3e0382631695b9
eljamm <83901271+eljamm@users.noreply.github.com>
2022-03-20 15:50:52 +0100

    Added HeartDisplay activity.

---
+++ app/src/main/java/com/sport/smartwatch/HeartDisplay.kt
@@ -0,0 +1,11 @@
+package com.sport.smartwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HeartDisplay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_heart_display)
+    }
+}
