a77b3486e4331fb38b6a8f9dd97e2bb919efdb34
brahimmasmudi <83924692+brahimmasmudi@users.noreply.github.com>
2022-03-20 16:30:04 +0100

    modified main activity

--- app/src/main/java/com/sport/smartwatch/MainActivity.kt
+++ app/src/main/java/com/sport/smartwatch/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.sport.smartwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    var btn= findViewById<Button>(R.id.btnSCAN)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn.setOnClickListener{
+             intent = Intent(this,HeartDisplay::class.java)
+             startActivity(intent)
+        }
     }
 }
